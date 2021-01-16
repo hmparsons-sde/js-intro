ddbc6d3456efecddbcfb28cb4bb1fb27ca468cd1
Holly Parsons <holly-parsons@outlook.com>
2021-01-16 11:08:04 -0600

    JS practice

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("Intro to JS");
+//console.log("Intro to JS");
+
+
+//Start variables
+//let firstName = 'Beyonce';
+//let lastName = 'Knowles';
+
+//console.log(firstName + " " + lastName);
+//console.log(`${firstName} ${lastName}`);
+
+//firstName = 'Holly';
+//lastName = 'Parsons';
+
+//console.log(firstName + " " + lastName);
+//console.log(`${firstName} ${lastName}`);
+//const favoriteFood = 'Zewditu';
+//console.log(favoriteFood);
+
+//End variables
+
+//const dog = 'bark';
+//console.log(typeof dog);
+
+//const answer = 33;
+//const negative = -28;
+
+//console.log(typeof answer, typeof negative);
+
+//const iLoveMyHusband = true;
+
+//console.log(typeof iLoveMyHusband);
+
+//End data types
+
+//Start Math
+
+
+
+//End Math
