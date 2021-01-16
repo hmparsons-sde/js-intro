89126ffa20dc0651c4a8a9faf6a0f27dd5018e81
Holly Parsons <holly-parsons@outlook.com>
2021-01-16 11:58:03 -0600

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Intro to JS");
+//console.log("Intro to JS");
+
+// function sayHello() {
+//   console.log("Hello!");
+// }
+
+// const sayHello = () => {
+//   console.log("Hello!");
+// }
+
+// sayHello();
+
+// function logsHello(name) {
+//   console.log(`Hello, ${name}!`);
+// }
+// logsHello('Holly');
+
+const addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log(sum);
+  return sum;
+}
+console.log(addsTwoNumbers(5,7));
