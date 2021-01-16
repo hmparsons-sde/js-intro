c5e965c28580278f66e566f17e4ac709167e7ce2
Holly Parsons <holly-parsons@outlook.com>
2021-01-16 13:09:21 -0600

    third commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Intro to JS");
+//console.log("Intro to JS");
+
+// function thisIsTrue(arg) {
+//   if (arg === 'nice') {
+//     console.log(`You are ${arg}`);
+//   } else {
+//     console.log('false');
+//   }
+// }
+
+// thisIsTrue('nice');
+
+const canDrive = (age) => {
+  if (!(age >= 15)) {
+    return true;
+  }
+
+    return false;
+  }
+
+console.log(canDrive(15));
